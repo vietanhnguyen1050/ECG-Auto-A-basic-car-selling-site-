67440365f5fd9e257682c5ee9dd7280c98373f9d
Viet Anh <vietanhnguyen1050@gmail.com>
2026-03-01 19:37:28 +0700

    demo

--- server/src/config/cloudinary.config.ts
+++ server/src/config/cloudinary.config.ts
@@ -1,10 +1,12 @@
 import { v2 as cloudinary } from "cloudinary";
 import { ENV } from "./environment.ts";
 
-cloudinary.config({
-  cloud_name: ENV.CLOUD_NAME,
-  api_key: ENV.CLOUD_API_KEY,
-  api_secret: ENV.CLOUD_API_SECRET,
-});
+if (ENV.CLOUD_NAME && ENV.CLOUD_API_KEY && ENV.CLOUD_API_SECRET) {
+  cloudinary.config({
+    cloud_name: ENV.CLOUD_NAME,
+    api_key: ENV.CLOUD_API_KEY,
+    api_secret: ENV.CLOUD_API_SECRET,
+  });
+}
 
 export { cloudinary };
